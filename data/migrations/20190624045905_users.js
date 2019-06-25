af25881d659195d57831cbb9e8cec942dbd911c2
azinoVo <azinovo22@gmail.com>
2019-06-24 23:31:52 -0500

    fixed migrations

--- data/migrations/20190624045905_users.js
+++ data/migrations/20190624045905_users.js
@@ -15,13 +15,12 @@ exports.up = function (knex, Promise) {
             tbl.string('restype', 125).notNullable();
             tbl.string('foodname', 125).notNullable();
             tbl.float('price', 125).notNullable();
-            tbl.integer('rating', 2).notNullable();
-            tbl.string('description', 1000);
+            tbl.integer('rating', 3).notNullable();
+            tbl.string('comment', 1000);
         });
 };
 
 exports.down = function (knex, Promise) {
     return knex.schema
         .dropTableIfExists('review')
-        .dropTableIfExists('games');
 };
--- data/seeds/002-review.js
+++ data/seeds/002-review.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('review').insert([
-        {user_id: 1, resname: 'The Big Cheesy', restype: 'american', foodname: 'cheese sandwich', price: 3.25, rating: 5, description: 'Comments are Here!'},
+        {user_id: 1, resname: 'The Big Cheesy', restype: 'american', foodname: 'cheese sandwich', price: 3.25, rating: 5, comment: 'Comments are Here!'},
     
       ]);
     });
